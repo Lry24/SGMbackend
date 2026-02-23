c0455ed1a819322f70bf0a2a63c10164950344bb
leroypn <panaleroy@gmail.com>
2026-02-23 14:35:55 +0000

    Initialisation du repository et du dépôt Git

---
+++ src/main/java/com/sgm/SGMbackend/SgMbackendApplication.java
@@ -0,0 +1,13 @@
+package com.sgm.SGMbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgMbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgMbackendApplication.class, args);
+	}
+
+}
